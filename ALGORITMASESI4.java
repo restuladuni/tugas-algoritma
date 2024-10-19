1f7d5dd17037fef9d19a4c30ad902b233e6b6319
restuladuni <ladunirestu@gmail.com>
2024-10-19 18:23:09 +0700

    Add files via upload

---
+++ ALGORITMASESI4.java
@@ -0,0 +1,65 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = new int[28];
+
+
+        numbers[0] = 1;
+        numbers[1] = 1;
+        numbers[2] = 2; 
+        numbers[3] = 3;
+        numbers[4] = 4;
+        numbers[5] = 5;
+        numbers[6] = 6;
+
+
+        numbers[7] = 1;
+        numbers[8] = 2;
+        numbers[9] = 3;
+        numbers[10] = 4;
+        numbers[11] = 5;
+        numbers[12] = 6;
+
+
+        numbers[13] = 1;
+        numbers[14] = 2;
+        numbers[15] = 3;
+        numbers[16] = 4;
+        numbers[17] = 5;
+        numbers[18] = 6;
+
+
+        numbers[19] = 1;
+        numbers[20] = 2;
+        numbers[21] = 3;
+        numbers[22] = 4;
+        numbers[23] = 5;
+        numbers[24] = 6;
+
+
+        numbers[25] = 2;
+        numbers[26] = 3;
+        numbers[27] = 4;
+        numbers[28] = 5;
+        numbers[29] = 6;
+        numbers[30] = 7;
+        
+
+        numbers[31] = 3;
+        numbers[32] = 4;
+        numbers[33] = 5;
+        numbers[34] = 6;
+        numbers[35] = 7;
+        numbers[36] = 8;
+
+
+        numbers[37] = 1;
+        numbers[38] = 1;
+        numbers[39] = 1;
+        numbers[40] = 1;
+
+
+        
+    }
+
+    
+}
